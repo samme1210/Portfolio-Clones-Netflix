0791c3914ff128d9487abb955bd59755271dc18a
samme1210 <samme1210@gmail.com>
2023-04-25 00:17:41 +0100

    test

--- pages/components/Thumbnail.tsx
+++ pages/components/Thumbnail.tsx
@@ -9,18 +9,19 @@ interface Props {
 }
 
 function Thumbnail({ movie }: Props) {
-
-  console.log("backdrop_path:", movie.backdrop_path);
-  console.log("poster_path:", movie.poster_path);
-
   return (
     <div
       className="relative h-28 min-w-[180px] cursor-pointer transition duration-200
     ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
     >
       <Image
+        //src={`https://image.tmdb.org/t/p/w500${
+        //  movie.backdrop_path || movie.poster_path
+        //}`}
         src={`https://image.tmdb.org/t/p/w500${
-          movie.backdrop_path ? movie.backdrop_path : movie.poster_path
+          movie.backdrop_path?.length > 0
+            ? movie.backdrop_path
+            : movie.poster_path
         }`}
         className="rounded-sm object-cover md:rounded"
         layout="fill"
