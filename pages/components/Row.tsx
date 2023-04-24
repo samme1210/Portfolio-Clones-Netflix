571ab4b92f8f1b5775393fd12cc7ee51620f1822
samme1210 <samme1210@gmail.com>
2023-04-24 23:20:09 +0100

    fix

--- pages/components/Row.tsx
+++ pages/components/Row.tsx
@@ -16,7 +16,7 @@ function Row({ title, movies }: Props) {
   const [isMoved, setIsMoved] = useState(false);
 
   const handleClick = (direction: string) => {
-    setIsMoved(true);
+    setIsMoved(true)
 
     if (rowRef.current) {
       const { scrollLeft, clientWidth } = rowRef.current;
@@ -32,34 +32,31 @@ function Row({ title, movies }: Props) {
 
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
-      <h2
-        className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition 
-      duration-200 hover:text-white md:text-2xl"
-      >
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
         {title}
       </h2>
       <div className="group relative md:-ml-2">
         <ChevronLeftIcon
-          className={`row__scroll--btn left-2 ${!isMoved && "hidden"}`}
-          onClick={() => handleClick("left")}
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && 'hidden'
+          }`}
+          onClick={() => handleClick('left')}
         />
-
         <div
+          className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
           ref={rowRef}
-          className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2"
         >
-          {movies.map((movie) =>  (
+          {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
-
         <ChevronRightIcon
-          className={`row__scroll--btn right-2`}
-          onClick={() => handleClick("right")}
+          className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          onClick={() => handleClick('right')}
         />
       </div>
     </div>
-  );
+  )
 }
 
-export default Row;
+export default Row
