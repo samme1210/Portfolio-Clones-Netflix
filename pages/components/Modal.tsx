c390632094aef7878f7313f413cd187544dde535
samme1210 <samme1210@gmail.com>
2023-04-26 19:45:10 +0100

    fin video modal

--- pages/components/Modal.tsx
+++ pages/components/Modal.tsx
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from "react";
 import MuiModal from "@mui/material/Modal";
 import { modalState, movieState } from "@/atoms/modalAtom";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { XMarkIcon } from "@heroicons/react/24/outline";
+import { HandThumbUpIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Element, Genre } from "@/typescript";
 import ReactPlayer from "react-player/lazy";
+import { FaPlay, FaVolumeOff, FaVolumeUp } from "react-icons/fa";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
   const [movie, setMovie] = useRecoilState(movieState);
   const [trailer, setTrailer] = useState("");
-  const [genres, setGenres] = useState<Genre[]>([])
-  const [muted, setMuted] = useState(false)
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [muted, setMuted] = useState(false);
 
   useEffect(() => {
     if (!movie) return;
@@ -27,12 +29,12 @@ function Modal() {
 
       if (data?.videos) {
         const index = data.videos.results.findIndex(
-          (element: Element) => element.type 
-          === "Trailer")
-          setTrailer(data.videos?.results[index]?.key)
+          (element: Element) => element.type === "Trailer"
+        );
+        setTrailer(data.videos?.results[index]?.key);
       }
-      if (data?.genre) {
-          setGenres(data.genres)
+      if (data?.genres) {
+        setGenres(data.genres);
       }
     }
 
@@ -43,10 +45,13 @@ function Modal() {
     setShowModal(false);
   };
 
-  console.log(trailer)
-
   return (
-    <MuiModal open={showModal} onClose={handleClose}>
+    <MuiModal
+      open={showModal}
+      onClose={handleClose}
+      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden
+    overflow-y-scroll rounded-md scrollbar-hide "
+    >
       <>
         <button
           onClick={handleClose}
@@ -60,11 +65,74 @@ function Modal() {
             url={`https://www.youtube.com/watch?v=${trailer}`}
             width="100%"
             height="100%"
-            style={{ position: 'absolute', top: '0', left: '0' }}
+            style={{ position: "absolute", top: "0", left: "0" }}
             playing
             muted={muted}
           />
+          <div
+            className="absolute bottom-10 flex w-full items-center justify-between
+            px-10"
+          >
+            <div className="flex space-x-2">
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e5]">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
 
+              <button className="modalButton">
+                <PlusIcon className="h-7 w-7" />
+              </button>
+
+              <button className="modalButton">
+                <HandThumbUpIcon className="h-7 w-7" />
+              </button>
+            </div>
+
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <FaVolumeOff className="h-6 w-6" />
+              ) : (
+                <FaVolumeUp className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm ">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div
+                className="flex h-4 items-center justify-center rounded border 
+                    border-white/40 px-1.5 text-xs"
+              >
+                HD
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+                <p className="w-5/6">{movie?.overview}</p>
+                <div className="flex flex-col sapce-y-3 text-sm">
+                    <div>
+                        <span className="text-[gray]">Genres: </span>
+                        {genres?.map((genre) => genre.name).join(', ')}
+                    </div>
+                    <div>
+                        <span className="text-[gray]">Language: </span>
+                        {movie?.original_language}
+                    </div>
+                    <div>
+                        <span className="text-[gray]">Total Votes: </span>
+                        {movie?.vote_count}
+                    </div>
+                </div>
+            </div>
+          </div>
         </div>
       </>
     </MuiModal>
--- pages/components/Row.tsx
+++ pages/components/Row.tsx
@@ -11,7 +11,6 @@ interface Props {
 }
 
 function Row({ title, movies }: Props) {
-  console.log(movies);
   const rowRef = useRef<HTMLDivElement>(null);
   const [isMoved, setIsMoved] = useState(false);
 
