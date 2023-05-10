977b1b268298ab6187094bddf90c22acd9aefd29
samme1210 <samme1210@gmail.com>
2023-05-10 11:05:49 +0100

    fin accounts

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,15 +1,24 @@
-import useAuth from "@/hooks/useAuth"
-import useSubscription from "@/hooks/useSubscription"
-import Head from "next/head"
-import Link from "next/link"
+import useAuth from "@/hooks/useAuth";
+import useSubscription from "@/hooks/useSubscription";
+import payments from "@/lib/stripe";
+import { getProducts, Product } from "@stripe/firestore-stripe-payments";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Membership from "./components/Membership";
 
-function Account() {
-    const {user} = useAuth()
-    const subscription = useSubscription(user)
+interface Props {
+  products: Product[];
+}
+
+function Account({ products }: Props) {
+  console.log(products);
+  const { user, logout } = useAuth();
+  const subscription = useSubscription(user);
 
   return (
     <div>
-        <Head>
+      <Head>
         <title>Account Settings - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -30,19 +39,73 @@ function Account() {
             className="cursor-pointer rounded"
           />
         </Link>
-
-        <main className="pt-24">
-            <div className="">
-                <h1 className="text-3xl md:text-4xl">Account</h1>
-                <div>
-                    <img src="https://rb.gy/4vfk4r" alt="" className="h-7 w-7" />
-                    <p>Member Since {subscription?.created}</p>
-                </div>
-            </div>
-        </main>
       </header>
+
+      <main
+        className="pt-24 mx-auto max-w-6xl px-5 pb-12 transition-all 
+      md:px-10"
+      >
+        <div className="flex flex-col gap-x-4 md:flex-row md:items-center">
+          <h1 className="text-3xl md:text-4xl">Account</h1>
+          <div className="-ml-0.5 flex items-center gap-x-1.5">
+            <img src="https://rb.gy/4vfk4r" alt="" className="h-7 w-7" />
+            <p className="text-xs font-semibold text-[#555]">
+              Member since {subscription?.created}
+            </p>
+          </div>
+        </div>
+
+        <Membership />
+
+        <div
+          className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 
+        md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0
+        md:pb-0"
+        >
+          <h4>Plan Details</h4>
+          {/* find current plan */}
+          <div className="">
+            {
+              products.filter(
+                (product) => product.id === subscription?.product
+              )[0]?.name
+            }
+          </div>
+          <p
+            className="cursor-pointer text-blue-500 hover-underline 
+          md:text:right "
+          >
+            Change plan
+          </p>
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
+          <h4 className="text-lg text-[gray]">Settings</h4>
+          <p
+            className="col-span-3 cursor-pointer text-blue-500 hover:underline"
+            onClick={logout}
+          >
+            Sign out of all devices
+          </p>
+        </div>
+
+      </main>
     </div>
-  )
+  );
 }
 
-export default Account
+export default Account;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await getProducts(payments, {
+    includePrices: true,
+    activeOnly: true,
+  })
+    .then((res) => res)
+    .catch((error) => console.log(error.message));
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
