0d08516fb6dd6dd14d1d615d4a32a1f4ac0d59d0
samme1210 <samme1210@gmail.com>
2023-05-11 22:31:57 +0100

    fixed syntax err

--- pages/components/Row.tsx
+++ pages/components/Row.tsx
@@ -7,7 +7,7 @@ import Thumbnail from "./Thumbnail";
 interface Props {
   title: string;
   //for firebase
-  movies: Movie | DocumentData[]
+  movies: Movie[] | DocumentData[]
 }
 
 function Row({ title, movies }: Props) {
